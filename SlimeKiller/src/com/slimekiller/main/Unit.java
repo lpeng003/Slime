328db8d11ab735840a475f07fa54584392515527
Unknown <youmslinky1@gmail.com>
2017-07-21 02:41:23 -0500

    add level up system

--- SlimeKiller/src/com/slimekiller/main/Unit.java
+++ SlimeKiller/src/com/slimekiller/main/Unit.java
@@ -13,6 +13,10 @@ public abstract class Unit {
 
     private int attack;
     private int defence;
+    private int levelUpExperience;
+
+
+
 
     Unit() {
         maxHealth = 1;
@@ -21,6 +25,15 @@ public abstract class Unit {
         mana = 1;
         attack = 1;
         defence = 1;
+    }
+    private int experienceRequired(int lvl) {
+        //for now the experience scaling is level^2, it should probably changed to something else
+        return lvl*lvl+10;
+    }
+    private void levelUp() {
+        level++;
+        attack += 1;
+        defence += 1;
     }
 
     public void setHealth(int hp) {health = hp;}
@@ -31,10 +44,12 @@ public abstract class Unit {
     public void setDefence(int def) {defence = def;}
     public void setLevel(int lvl) {level = lvl;}
     public void setExperience(int exp) {experience = exp;}
+    public void setLevelUpExperience() {levelUpExperience = experienceRequired(level);}
 
     public int getAttack() {return attack;}
     public int getDefence() {return defence;}
     public int getHealth() {return health;}
+    public int getExperience() {return experience;}
 
     public void addHealth(int num) {
             health = (num + health >= maxHealth) ? maxHealth : (health + num);
@@ -47,6 +62,16 @@ public abstract class Unit {
     }
     public void subMana(int num) {
         mana = (mana - num <= 0) ? 0 : (mana - num);
+    }
+    public void addExperience(int exp){
+        experience += exp;
+        while (experience >= levelUpExperience) {
+            levelUp();
+            experience -= levelUpExperience;
+            levelUpExperience = experienceRequired(level);
+            System.out.println("leveled up to level "+level+"!");
+        }
+        System.out.println("you now have "+experience+"/"+levelUpExperience+"xp");
     }
 
     public boolean isAlive() {return health > 0;}
